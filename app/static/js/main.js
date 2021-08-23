eed11e34099d6cd30f547904ef0b9ee263d57a0a
AdrianM0Hdz <morthem@LAPTOP-BAVC5R5S.localdomain>
2021-08-23 15:07:17 -0500

    stuck with rendering pokemon functionality

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,4 +1,5 @@
 import getPokemon from './utils/getPokemon.js';
+import renderPokemon from './utils/renderPokemon.js';
 
 const textBox = document.querySelector('#pokemon-name');
 textBox.value = '';
@@ -6,6 +7,6 @@ const button = document.querySelector('#submit');
 button.addEventListener('click', async (event) => {
     let pokemonName = textBox.value;
     const pokemon = await getPokemon(pokemonName);
-    console.log(pokemon);
+    await renderPokemon(pokemon);
     textBox.value = '';
 });
--- app/static/js/utils/renderPokemon.js
+++ app/static/js/utils/renderPokemon.js
@@ -1,5 +1,7 @@
-const renderPokemom = async (pokemon) => {
-
+const renderPokemon = async (pokemonData) => {
+    const pokemon = document.querySelector('#pokemon');
+    pokemon.firstChild.innerHTML = pokemonData.name;
+    pokemon.lastChild.src = pokemonData.sprites.front_default;
 }
 
 export default renderPokemon;
