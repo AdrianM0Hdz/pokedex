47716ccc398cdad5a548d3117833a50ada84f36c
AdrianM0Hdz <morthem@LAPTOP-BAVC5R5S.localdomain>
2021-08-24 16:08:42 -0500

    changed implememntation of the pokemon search to a form element and fixed the renderPokemon function

--- app/static/js/utils/renderPokemon.js
+++ app/static/js/utils/renderPokemon.js
@@ -1,7 +1,12 @@
 const renderPokemon = async (pokemonData) => {
-    const pokemon = document.querySelector('#pokemon');
-    pokemon.firstChild.innerHTML = pokemonData.name;
-    pokemon.lastChild.src = pokemonData.sprites.front_default;
+    const pokemonContainer = document.querySelector('#pokemon');
+    const image = document.createElement('img');
+    image.src = pokemonData.sprites.front_default;
+    const name = document.createTextNode(pokemonData.species.name); 
+    const br = document.createElement('br');
+    pokemonContainer.appendChild(image);
+    pokemonContainer.appendChild(name);
+    pokemonContainer.appendChild(br);
 }
 
 export default renderPokemon;
